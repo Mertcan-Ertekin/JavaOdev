caf4d021aefd903b1584a2f68b91951d692beced
MERTCAN ERTEKİN <201811066@mostem.onmicrosoft.com>
2021-06-01 15:09:29 +0300

    HRMS veri tabanı projesi

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/JobTitleDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.northwind.entities.concretes.JobTitles;
-
-public interface JobTitleDao extends JpaRepository<JobTitles, Integer>{
-
-}
